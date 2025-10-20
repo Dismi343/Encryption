a6213a6b7d599066c754c1a9c0ac2cd93d5109f8
Yushan Hettiarachchi <130626574+Dismi343@users.noreply.github.com>
2025-10-20 15:00:56 +0530

    cipher-decrption

--- VernamCipherEncryption.java
+++ VernamCipherEncryption.java
@@ -8,6 +8,7 @@ public class VernamCipherEncryption {
         char[] alpha={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        ArrayList<Integer> keyList = new ArrayList<>();
 
         String plainText=scanner.nextLine().toUpperCase();
         StringBuilder cipherText=new StringBuilder();
@@ -18,17 +19,30 @@ public class VernamCipherEncryption {
                     if(plainArray[i]==alpha[j]){
                         int randomIndex=random.nextInt(100);
                         int cipherIndex=(j+randomIndex)%26;
+                        keyList.add(randomIndex);
                         System.out.println("Cipher Index: "+cipherIndex+" Random Index: "+randomIndex);
                         cipherText.append(alpha[cipherIndex]);
                         break;
                     }
             }
     }
-
-    
-
     System.out.println("\nCipher Text: "+cipherText.toString());
 
+    StringBuilder decryptedText = new StringBuilder();
+    char[] cipherArray = cipherText.toString().toCharArray();
+        
+        for(int i=0; i<cipherArray.length; i++){
+            for(int j=0; j<alpha.length; j++){
+                if(cipherArray[i] == alpha[j]){
+                    int originalIndex = Math.floorMod(j - keyList.get(i), 26);
+                    decryptedText.append(alpha[originalIndex]);
+                    break;
+                }
+            }
+        }
+
+    System.out.println("Decrypted Text: " + decryptedText.toString());
+    
 }
 }
 
